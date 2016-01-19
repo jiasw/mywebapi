144fdad7934bfa5005390f6920ff661b842c4a37
jiasw <jswjia@163.com>
2016-01-19 22:14:36 +0800

    添加数据令牌验证

--- MvcApiDemo/Base/CustomAuthAttribute.cs
+++ MvcApiDemo/Base/CustomAuthAttribute.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using MvcApiDemo.DB;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Web;
 using System.Web.Http;
 using System.Web.Http.Controllers;
@@ -14,7 +17,30 @@ namespace MvcApiDemo.Base
 
         public override void OnAuthorization(HttpActionContext actionContext)
         { 
-            
+            //验证Header是否存在
+            if (actionContext.Request.Headers.Contains("UserID"))
+            {
+                //判断当前userID是否存在登陆令牌，如果不存在请重新登陆
+                string userid = actionContext.Request.Headers.GetValues("UserID").First();
+                byte[] result=RedisHelper.DB.Get(userid);
+                if (result!=null &&result.Length>0)
+                {
+                    RedisHelper.DB.SetEx(userid,  30, System.Text.Encoding.Default.GetBytes(userid));
+                }
+                else
+                { 
+                    //不存在需要重新登陆
+                    actionContext.Response = new System.Net.Http.HttpResponseMessage(HttpStatusCode.Conflict);
+                    actionContext.Response.Content = new StringContent("请登录");
+                    actionContext.Response.Headers.Add("rekey", "不存在令牌");
+                }
+            }
+            else
+            {
+                actionContext.Response = new System.Net.Http.HttpResponseMessage(HttpStatusCode.Conflict);
+                actionContext.Response.Content=new StringContent("请登录");
+                actionContext.Response.Headers.Add("rekey", "错误");
+            }
         }
     }
 }
---
+++ MvcApiDemo/DB/RedisHelper.cs
@@ -0,0 +1,33 @@
+﻿using ServiceStack.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApiDemo.DB
+{
+    public class RedisHelper
+    {
+        private static string host = "localhost";
+        private static string elementKey = "testKeyRedis";
+        private static object objLocker = new object();
+        private static RedisClient _db = null;
+        /// <summary>
+        /// SqlserverDbsession
+        /// </summary>
+        public static RedisClient DB
+        {
+            get
+            {
+                if (_db == null)
+                {
+                    lock (objLocker)
+                    {
+                        _db = new RedisClient(host, 6379, "jiasw");
+                    }
+                }
+                return _db;
+            }
+        }
+    }
+}
